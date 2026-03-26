b26bd8418c45d1f5506a660a2c14a49a7d42e9b6
antso.tech <hei.antso.2007@gmail.com>
2026-03-26 20:16:58 +0300

    first commit

---
+++ src/main/java/hei/school/dish_application/DishApplication.java
@@ -0,0 +1,13 @@
+package hei.school.dish_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishApplication.class, args);
+	}
+
+}
